efc0febe048a7b7bcd8196102aa1e394690cc036
Marco Antônio <ma.souza.junior@gmail.com>
2023-06-02 10:21:08 -0300

    enh: update welcome text

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,8 +13,7 @@ export default function Home() {
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
         <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
+          Welcome to your job board assistant!
         </p>
       </div>
     </>
